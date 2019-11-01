f91bde67bb4718383fba85b79b7e19f6d42a6d63
igmrrf <francis.igbiriki.fi@gmail.com>
2019-11-01 16:36:43 +0100

    Updates

---
+++ precache-manifest.096e81990931aaa774eb50e74f436f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e1f1f6967fc21ba4f34b983721020e",
+    "url": "/fan_movies/index.html"
+  },
+  {
+    "revision": "804273ee8424ada14073",
+    "url": "/fan_movies/static/css/main.18fae0cc.chunk.css"
+  },
+  {
+    "revision": "ee23be645a8e9a1aaf1c",
+    "url": "/fan_movies/static/js/2.4d8eb4e9.chunk.js"
+  },
+  {
+    "revision": "804273ee8424ada14073",
+    "url": "/fan_movies/static/js/main.e9e2413a.chunk.js"
+  },
+  {
+    "revision": "71acf2c6d4c0c032a706",
+    "url": "/fan_movies/static/js/runtime-main.7ee9df63.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/fan_movies/static/media/logo.25bf045c.svg"
+  }
+]);
